556f3ad5cba2ed072ef2c7c9fab536a8a6e5a242
Stadnik Andrii <stadnikandriy1@gmail.com>
2021-11-16 18:09:04 +0200

    Working 2nd iteration

--- sec/secondary.py
+++ sec/secondary.py
@@ -1,50 +1,51 @@
-from flask import Flask,request, jsonify
-import time
-import random
 import json
+import logging
 import os
+import random
+import time
 
+from flask import Flask, jsonify, request
+
+log = logging.getLogger("werkzeug")
+log.setLevel(logging.ERROR)
 
 
 app = Flask(__name__)
 
 MESSAGES_LIST = []
 
-@app.route("/",methods=["GET","POST"])
+
+@app.route("/", methods=["GET", "POST"])
 async def msgs_listener():
-    if request.method in ['DELETE', 'PUT']:
-        return jsonify(isError=True,
-                       message=f'Use GET or POST methods.',
-                       statusCode=400), 400
+    if request.method in ["DELETE", "PUT"]:
+        return (
+            jsonify(isError=True, message=f"Use GET or POST methods.", statusCode=400),
+            400,
+        )
     # time.sleep(random.randint(1, 10))
     try:
         time.sleep(int(os.environ["SLEEP_TIMEOUT"]))
     except (KeyError, ValueError):
         time.sleep(random.randint(1, 10))
 
-    if request.method == 'POST':
+    if request.method == "POST":
         if request.json is None:
             print("Wrong request")
-            return jsonify(isError=True,
-                           message='Use JSON please')
+            return jsonify(isError=True, message="Use JSON please")
         data = request.json
-        if 'msg' in data:
+        if "msg" in data:
             print(f'added message {data["msg"]}')
-            MESSAGES_LIST.append(data['msg'])
-            return jsonify(isError=False,
-                           message="Success",
-                           statusCode=200,
-                           data=data), 200
+            MESSAGES_LIST.append(data["msg"])
+            return (
+                jsonify(isError=False, message="Success", statusCode=200, data=data),
+                200,
+            )
         else:
-            return jsonify(isError=True,
-                           message='Use json key "msg" for POST request.')
+            return jsonify(isError=True, message='Use json key "msg" for POST request.')
 
-    if request.method == 'GET':
+    if request.method == "GET":
         print(f"Messages: {MESSAGES_LIST}")
-        return jsonify(isError=False,
-                       message=MESSAGES_LIST,
-                       statusCode=200), 200
-
+        return jsonify(isError=False, message=MESSAGES_LIST, statusCode=200), 200
 
 
 if __name__ == "__main__":
